e5a89a517545538c778c9588ba7f4f4f33021352
AkifhanIlgaz <101044332+AkifhanIlgaz@users.noreply.github.com>
2022-08-15 20:37:51 +0300

    Added First Case

---
+++ JavascriptCase/JavascriptCase.js
@@ -0,0 +1,17 @@
+function oddishOrEvenish(number) {
+  let sum = 0;
+  number = number.toString();
+
+  for (let i = 0; i < number.length; i++) {
+    sum += +number[i];
+  }
+
+  return sum % 2 === 0 ? "Even" : "Odd";
+}
+
+console.log(oddishOrEvenish(43));
+console.log(oddishOrEvenish(373));
+console.log(oddishOrEvenish(4433));
+console.log(oddishOrEvenish(45));
+console.log(oddishOrEvenish(44));
+console.log(oddishOrEvenish(12));
