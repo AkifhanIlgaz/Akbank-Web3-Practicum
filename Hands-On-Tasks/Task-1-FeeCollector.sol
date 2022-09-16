763739637b9fa9e91e2a4b8b33df2743a35b942a
AkifhanIlgaz <101044332+AkifhanIlgaz@users.noreply.github.com>
2022-09-17 02:17:04 +0300

    Update Task-1-FeeCollector.sol

--- Hands-On-Tasks/Task-1-FeeCollector.sol
+++ Hands-On-Tasks/Task-1-FeeCollector.sol
@@ -3,22 +3,23 @@
 pragma solidity ^0.8.7;
 
 contract FeeCollector {
-  address public owner;
-  uint256 public balance;
+  address public owner; 
+  uint256 public balance; // Since we only deposit ether, we can access balance of the contract by address(this).balance
 
   constructor() {
-    owner = msg.sender;
+    owner = msg.sender; // Owner of the contract is the address that deploys it
   }
-
+  
+  // receive function is used to handle when the contract receives ether
   receive() external payable {
-    balance += msg.value;
+    balance += msg.value; // msg.value is the ether amount(in wei) that is sent with the transaction
   }
 
   function withdraw(uint256 amount, address payable destinationAddress) public {
-    require(msg.sender == owner, "Only owner can withdraw from this contract.");
-    require(amount <= balance, "Insufficient funds");
+    require(msg.sender == owner, "Only owner can withdraw from this contract."); // Check if the msg.sender is the current owner of the contract
+    require(amount <= balance, "Insufficient funds"); // Check if there are enough amount of ether in the contract
 
-    destinationAddress.transfer(amount);
+    destinationAddress.transfer(amount); // Send ether to the destination address
     balance -= amount;
   }
 }
